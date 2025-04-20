2a213240f0b3ac1a23cdd036cb95b66cdf3bbfd7
MelViolin <153835343+melviolin@users.noreply.github.com>
2025-04-20 16:45:46 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, request, send_file
+from weasyprint import HTML
+import io
+
+app = Flask(__name__)
+
+@app.route("/generate", methods=["POST"])
+def generate_pdf():
+    html_content = request.get_data(as_text=True)
+    pdf_bytes = HTML(string=html_content).write_pdf()
+    return send_file(io.BytesIO(pdf_bytes), mimetype="application/pdf", as_attachment=True, download_name="documento.pdf")
