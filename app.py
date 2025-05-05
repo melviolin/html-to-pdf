078fd0f95ecaa70347ffd37138629bdabeb98ceb
MelViolin <153835343+melviolin@users.noreply.github.com>
2025-05-05 18:58:30 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,9 +11,22 @@ def generate_pdf():
     
     # Configurar wkhtmltopdf (ruta estándar en la mayoría de servidores Linux)
     config = pdfkit.configuration(wkhtmltopdf="/usr/bin/wkhtmltopdf")
-    
-    # Generar PDF
-    pdf_bytes = pdfkit.from_string(html_content, False, configuration=config)
+
+
+    # Opciones para el PDF
+    options = {
+        'margin-top': '15mm',
+        'margin-right': '15mm',
+        'margin-bottom': '15mm',
+        'margin-left': '15mm',
+        'encoding': 'UTF-8',
+        'page-size': 'A4',
+        'print-media-type': '',
+    }
+
+    # Generar el PDF con márgenes
+    pdf_bytes = pdfkit.from_string(html_content, False, configuration=config, options=options)
+
     pdf_output = io.BytesIO(pdf_bytes)
 
     return send_file(pdf_output, mimetype='application/pdf', as_attachment=True, download_name="documento.pdf")
